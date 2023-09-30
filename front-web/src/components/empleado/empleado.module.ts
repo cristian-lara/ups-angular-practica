2759d38fef52a24bf69a5bed268fc0fc8d9ee9fe
Cristian Lara <cristian89lara@gmail.com>
2023-09-29 22:21:43 -0600

    feat: componentes creados

--- front-web/src/components/empleado/empleado.module.ts
+++ front-web/src/components/empleado/empleado.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { FormularioEmpleadoComponent } from './formulario-empleado/formulario-empleado.component';
+import { ListaEmpleadoComponent } from './lista-empleado/lista-empleado.component';
 
 
 
 @NgModule({
     declarations: [
-        EmpleadoComponent
+        EmpleadoComponent,
+        FormularioEmpleadoComponent,
+        ListaEmpleadoComponent
     ],
     exports: [
         EmpleadoComponent
---
+++ front-web/src/components/empleado/formulario-empleado/formulario-empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-empleado',
+  templateUrl: './formulario-empleado.component.html',
+  styleUrls: ['./formulario-empleado.component.scss']
+})
+export class FormularioEmpleadoComponent {
+
+}
